c3a0d2a1812e4f31f73a30542da81be20136ef84
liaoyueyue <973044869@qq.com>
2023-10-25 23:05:32 +0800

    update: 20231025

---
+++ myblog-ssm/src/main/resources/static/js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".nav").load("common/navigation.html");
+});
+
+function logout() {
+  if (confirm("确认注销？")) {
+    $.ajax({
+      url: "/user/logout",
+      type: "post",
+      data: {},
+      success: function (result) {
+        if (result != null && result.code == 200 && result.data == 1) {
+          location.href = "/login.html";
+        } else {
+          alert("注销失败");
+        }
+      },
+    });
+  }
+}
