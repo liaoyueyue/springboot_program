4235b26497ef38d88af4f8cbc9719efdcfa17389
liaoyueyue <973044869@qq.com>
2023-11-09 23:12:14 +0800

    update: updateStyle

--- myblog-ssm/src/main/java/com/example/myblogssm/config/AppConfig.java
+++ myblog-ssm/src/main/java/com/example/myblogssm/config/AppConfig.java
@@ -23,7 +23,7 @@ public class AppConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/editor.md/**", "/images/**", "/js/**")
                 .excludePathPatterns("/favicon.ico", "/login.html", "/blog_list_comm.html", "/blog_detail.html", "/common/navigation.html")
-                .excludePathPatterns("/user/login", "/user/register", "/user/showinfobyid", "/email/sendverificationcode")
+                .excludePathPatterns("/user/login", "/user/register", "/user/showinfobyid","/user/showinfo", "/email/sendverificationcode")
                 .excludePathPatterns("/article/showinfo", "/article/showinfolistbypage");
     }
 
--- myblog-ssm/src/main/resources/static/js/common.js
+++ myblog-ssm/src/main/resources/static/js/common.js
@@ -1,6 +1,22 @@
 // 导航栏初始化
+// window.onload 必须等到页面内包括图片的所有元素加载完毕后才能执行；
+// $(document).ready() 是 dom 结构绘制完毕后就执行，不必等到加载完毕。
 $(document).ready(function () {
-  $(".nav-wrapper").load("/common/navigation.html");
+  $(".nav-wrapper").load("/common/navigation.html", function () {
+    // 导航栏资源加载完毕后执行
+    $.ajax({
+      url: "/user/showinfo",
+      type: "post",
+      data: {},
+      success: function (result) {
+        if (result != null && result.code == 200) {
+          $("#nav-username").text(result.data.username);
+          $("#nav-photo").attr("src", result.data.photo);
+          $("#nav-logout").show();
+        }
+      },
+    });
+  });
 });
 
 function logout() {
