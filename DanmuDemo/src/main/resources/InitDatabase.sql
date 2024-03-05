8af1df4b62b582c415c8c8e9f9ae04d73066a5ea
liaoyueyue <973044869@qq.com>
2024-03-06 01:42:04 +0800

    update: 20240306

--- DanmuDemo/src/main/resources/InitDatabase.sql
+++ DanmuDemo/src/main/resources/InitDatabase.sql
@@ -3,24 +3,24 @@ create database danmu_demo character set utf8mb4;
 
 use danmu_demo;
 
-drop table if exists users;
-create table users
+drop table if exists user;
+create table user
 (
     id          int primary key auto_increment,
     name        varchar(64),
     password    varchar(64),
     create_time timestamp default now()
 );
-insert into users(name, password)
+insert into user(name, password)
 values ('alice', '123');
 
-drop table if exists danmus;
-create table danmus
+drop table if exists danmu;
+create table danmu
 (
     id          int primary key auto_increment,
     text        varchar(64),
     username    varchar(64),
     create_time timestamp default now()
 );
-insert into danmus(text, username)
+insert into danmu(text, username)
 values ('这是一条弹幕', 'alice');
