f7846093f1c4b17d8cd4ef9ad152955ba313bb20
liaoyueyue <973044869@qq.com>
2024-03-07 11:56:09 +0800

    add: 20240307

---
+++ DanmuDemo/README.md
@@ -0,0 +1,15 @@
+# 视频弹幕网站
+
+### 技术栈：
+
+前端
+
+* Artplayer 优雅的视频播放器 [Artplayer文档](https://artplayer.org/document/)
+
+* Layui 开源 Web UI 组件库 [Layui 文档](https://layui.dev/docs/2/)
+
+后端
+
+* SpringBoot
+* MySQL
+* MyBatis
--- OrderDemo/src/main/java/org/example/orderdemo/mapper/UserMapper.java
+++ OrderDemo/src/main/java/org/example/orderdemo/mapper/UserMapper.java
@@ -1,8 +1,7 @@
 package org.example.orderdemo.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.example.orderdemo.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
@@ -10,7 +9,7 @@ import java.util.List;
 * @author liaoyueyue
 * @description 针对表【user】的数据库操作Mapper
 * @createDate 2024-02-29 19:33:22
-* @Entity org.example.orderdemo.entity.User
+* @Entity entity.com.example.orderdemo.User
 */
 @Mapper
 public interface UserMapper {
